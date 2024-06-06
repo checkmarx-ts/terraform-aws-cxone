8e1d0bacdcd1fd156ad5a406066fda12fd6906dc
benjaminstokes <stokesbo@gmail.com>
2024-06-06 17:18:36 -0400

    fix key generation

--- modules/cxone-install/keys.tf
+++ modules/cxone-install/keys.tf
@@ -1,11 +1,13 @@
+# core_configuration_encryption_key is 64 character hex value
 resource "random_password" "core_configuration_encryption_key" {
-  count       = var.core_configuration_encryption_key == null ? 1 : 0
-  length      = 64
-  special     = false
-  min_special = 0
-  min_upper   = 1
-  min_lower   = 1
-  min_numeric = 1
+  count            = var.core_configuration_encryption_key == null ? 1 : 0
+  length           = 64
+  special          = true
+  override_special = "abcdef0123456789"
+  min_special      = 64
+  min_upper        = 0
+  min_lower        = 0
+  min_numeric      = 0
 }
 
 resource "random_password" "sca_client_secret" {
@@ -57,7 +59,7 @@ resource "random_password" "integrations_repos_manager_github_tenant_key" {
 }
 resource "random_password" "integrations_repos_manager_gitlab_tenant_key" {
   count       = var.integrations_repos_manager_gitlab_tenant_key == null ? 1 : 0
-  length      = 32
+  length      = 8
   special     = false
   min_special = 0
   min_upper   = 1
